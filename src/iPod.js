5d4f06c03af7c6907a4f234d87e9e0bf766570b9
Bhavya Tanwar <bhavyatanwar@Bhavyas-MacBook-Air.local>
2021-05-08 20:47:46 +0530

    basic structure of ipod

---
+++ src/iPod.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class Ipod extends React.Component{
+    render() {
+        return (
+            <div>
+                <audio className = "audio-element">
+                    <source></source>
+                </audio>
+                <div id="inner-container">
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+export default Ipod;
